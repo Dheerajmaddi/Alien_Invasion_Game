fe6436de36aedb3807fcdfc5bc9969054c2a4d5c
Dheeraj Maddi <dheerajmaddi@gmail.com>
2025-05-16 01:28:01 -0500

    Created a two modules, to generate a game window.

---
+++ alien_invasion.py
@@ -0,0 +1,39 @@
+import sys
+import pygame
+
+from settings import Settings
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+    
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        # Add a clock to monitor the time
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption('Alien Invasion')
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            # Make the most recently drawn screen available.
+            pygame.display.flip()
+            # Make the clock tick
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    alien_invasion_game = AlienInvasion()
+    alien_invasion_game.run_game()
